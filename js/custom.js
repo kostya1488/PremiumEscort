8875f2a11866e156e1c8cd93618c0a3c68cf9400
kostya <matvienkoigor956@gmail.com>
2020-06-16 20:45:09 +0300

    added working feedback form with file, added shows image preview area.

--- js/custom.js
+++ js/custom.js
@@ -21,38 +21,6 @@ $(document).ready(function() {
         $('#s-icons').tooltip('show');
     }
 });
-// Input file
-var inputs = document.querySelectorAll('.inputfile');
-Array.prototype.forEach.call(inputs, function(input) {
-    var label = input.nextElementSibling,
-        labelVal = label.innerHTML;
-    input.addEventListener('change', function(e) {
-        var fileName = '';
-        if (this.files && this.files.length > 1)
-            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-        else
-            fileName = e.target.value.split('\\').pop();
-        if (fileName)
-            label.querySelector('span').innerHTML = fileName;
-        else
-            label.innerHTML = labelVal;
-    });
-});
-
-// Подсвечивание активной области меню
-jQuery(window).scroll(function() {
-    var $sections = $('section');
-    $sections.each(function(i, el) {
-        var top = $(el).offset().top - 150;
-        var bottom = top + $(el).height();
-        var scroll = $(window).scrollTop();
-        var id = $(el).attr('id');
-        if (scroll > top && scroll < bottom) {
-            $('a.is-selected').removeClass('is-selected');
-            $('a[href="#' + id + '"]').addClass('is-selected');
-        }
-    });
-});
 
 // Fixed menu
 $(window).scroll(function() {
@@ -65,17 +33,7 @@ $(window).scroll(function() {
         }
     }
 });
-// Hide #
-$('.hh').on('click', function(e) {
 
-    e.preventDefault();
-
-    var target = $(this).attr('href'),
-        offset = $(target).offset().top;
-
-    $(document).scrollTop(offset);
-
-});
 // Right icons (replace icon)
 $(document).ready(function() {
     $('#s-icons').click(function() {
@@ -86,14 +44,82 @@ $(document).ready(function() {
         $("i", this).toggleClass("fa-plus fa-times");
     });
 });
-// Hide elements in menu after scroll
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 150) {
-        $('.navbar').addClass('scroll');
+
+// Read more for reviews
+function readMore(iddots, idmore, idmyBtn) {
+    var dots = document.getElementById(iddots);
+    var moreText = document.getElementById(idmore);
+    var btnText = document.getElementById(idmyBtn);
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Показать полностью";
+        moreText.style.display = "none";
     } else {
-        $('.navbar').removeClass('scroll')
+        dots.style.display = "none";
+        btnText.innerHTML = "Скрыть";
+        moreText.style.display = "inline";
     }
-});
+}
+
+// Input file
+// var inputs = document.querySelectorAll('.inputfile');
+// Array.prototype.forEach.call(inputs, function(input) {
+//     var label = input.nextElementSibling,
+//         labelVal = label.innerHTML;
+//     input.addEventListener('change', function(e) {
+//         var fileName = '';
+//         if (this.files && this.files.length > 1)
+//             fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+//         else
+//             fileName = e.target.value.split('\\').pop();
+//         if (fileName)
+//             label.querySelector('span').innerHTML = fileName;
+//         else
+//             label.innerHTML = labelVal;
+//     });
+// });
+
+// Подсвечивание активной области меню
+// jQuery(window).scroll(function() {
+//     var $sections = $('section');
+//     $sections.each(function(i, el) {
+//         var top = $(el).offset().top - 150;
+//         var bottom = top + $(el).height();
+//         var scroll = $(window).scrollTop();
+//         var id = $(el).attr('id');
+//         if (scroll > top && scroll < bottom) {
+//             $('a.is-selected').removeClass('is-selected');
+//             $('a[href="#' + id + '"]').addClass('is-selected');
+//         }
+//     });
+// });
+
+
+// Hide #
+// $('.hh').on('click', function(e) {
+
+//     e.preventDefault();
+
+//     var target = $(this).attr('href'),
+//         offset = $(target).offset().top;
+
+//     $(document).scrollTop(offset);
+
+// });
+
+
+
+
+
+// Hide elements in menu after scroll
+// $(window).scroll(function() {
+//     if ($(window).scrollTop() > 150) {
+//         $('.navbar').addClass('scroll');
+//     } else {
+//         $('.navbar').removeClass('scroll')
+//     }
+// });
 
 
 
@@ -133,45 +159,81 @@ function ajaxFormRequest(form_id, url) {
     })
 }
 
-jQuery(document).ready(function() {
-    // var errorTxt = 'Ошибка отправки';
-    jQuery("#fprofile").validate({
-        submitHandler: function(form) {
-            var form = document.forms.fprofile,
-                formData = new FormData(form),
-                xhr = new XMLHttpRequest();
+// jQuery(document).ready(function() {
+//     jQuery("#fprofile").validate({
+//         submitHandler: function(form) {
+//             var form = document.forms.fprofile,
+//                 formData = new FormData(form),
+//                 xhr = new XMLHttpRequest();
 
-            xhr.open("POST", "php/form-profile.php");
+//             xhr.open("POST", "php/form-profile.php");
 
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        hideShowSection('fprofile', 'anketaAfterSend');
-                        return false
-                    }
+//             xhr.onreadystatechange = function() {
+//                 if (xhr.readyState == 4) {
+//                     if (xhr.status == 200) {
+//                         hideShowSection('fprofile', 'anketaAfterSend');
+//                         return false
+//                     }
+//                 }
+//             };
+//             xhr.send(formData);
+//         }
+//     });
+// })
+$(function() {
+    document.getElementById('fprofile').addEventListener('submit', function(evt) {
+        var http = new XMLHttpRequest(),
+            f = this;
+        var th = $(this);
+        evt.preventDefault();
+        http.open("POST", "php/form-profile.php", true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                hideShowSection('fprofile', 'anketaAfterSend');
+                // alert(http.responseText);
+                if (http.responseText.indexOf(f.name.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (name)
+                    th.trigger("reset");
                 }
-            };
-            xhr.send(formData);
+            }
         }
-    });
-})
+        http.onerror = function() {
+            alert('Ошибка, попробуйте еще раз');
+        }
+        http.send(new FormData(f));
+    }, false);
 
-// Read more for reviews
-function readMore(iddots, idmore, idmyBtn) {
-    var dots = document.getElementById(iddots);
-    var moreText = document.getElementById(idmore);
-    var btnText = document.getElementById(idmyBtn);
+});
+//---------------------
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Показать полностью";
-        moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Скрыть";
-        moreText.style.display = "inline";
+
+function shows_selected_file(id) {
+    document.getElementById(id).addEventListener('change', handleFileSelect, false);
+
+    function handleFileSelect(evt) {
+        var file = evt.target.files; // FileList object
+        var f = file[0];
+
+        var reader = new FileReader();
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+            return function(e) {
+
+                var span_in_lebel = document.createElement('span');
+                span_in_lebel.innerHTML = ['<img class="thumb" title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
+
+                var lebel_for_input = $("[for=" + id + "]")[0];
+                remove_ContentAndPadding(lebel_for_input);
+
+                lebel_for_input.insertBefore(span_in_lebel, null);
+
+            };
+        })(f);
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
     }
+
 }
+
 
 //-------------------------------------------------------------------------------------------------------------------
 
@@ -179,4 +241,9 @@ function hideShowSection(elemHide, elemshow) {
     document.getElementById(elemHide).style.display = 'none';
     document.getElementById(elemshow).style.display = 'block';
 
+}
+
+function remove_ContentAndPadding(element) {
+    element.innerHTML = "";
+    element.style.padding = "0";
 }
--- php/form-profile.php
+++ php/form-profile.php
@@ -1,90 +1,81 @@
 <?php
-
-// if($_POST)
-// {
-//     $instagram = '';
-//     if(empty($_POST['insta'])) { $instagram = "Не указала"; } else { $instagram = $_POST['insta']; }
-//     $exp = '';
-//     if(empty($_POST['exp'])) { $exp = "Не указала"; } else { $exp = $_POST['exp']; }
-//     $to = "matvienkoigor956@gmail.com";
-//     $subject = "Анкета (Premium Escort)";
-//     $message = '<span style="font-weight:bold;color:#007610;">Новая анкета, заполненная на сайте</span><br>
-//     Имя: <span style="font-weight:bold;color:#B38600;">'.$_POST['name'].'</span><br>
-//     Телефон: <span style="font-weight:bold;color:#B38600;"> '.$_POST['phone'].'</span><br>
-//     Город: <span style="font-weight:bold;color:#B38600;"> '.$_POST['city'].'</span><br>
-//     Возраст: <span style="font-weight:bold;color:#B38600;"> '.$_POST['age'].'</span><br>
-//     Instagram: <span style="font-weight:bold;color:#B38600;"> '.$instagram.'</span><br>
-//     Опыт в эскорте: <span style="font-weight:bold;color:#B38600;">'.$exp.'</span>';
-//     $headers = "Content-type: text/html; charset=UTF-8 \r\n";
-//     $headers .= "From: <agent@premuim-escort.com>\r\n";
-
-//     $result = mail($to, $subject, $message, $headers);
-
-//     if ($result){ echo "<center>Если ты нам подходишь, мы свяжемся с тобой для обсуждения всех деталей сотрудничества. Работа в досуге для девушек – это твой шанс стать финансово независимой, успешной и, быть может, даже знаменитой.</center>"; }
-// }
-$to = 'matvienkoigor956@gmail.com';
-
-if ( isset( $_POST['sendMail'] ) ) {
-	$name = substr( $_POST['name'], 0, 64 );
-	$age = substr( $_POST['age'], 0, 64 );
-	$city = substr( $_POST['city'], 0, 64 );
-	$tel = substr( $_POST['tel'], 0, 64 );
-    $insta = substr( $_POST['insta'], 0, 64 );
-	$exp = substr( $_POST['exp'], 0, 64 );
-
-if($_FILES)
-{
-	$filepath = array();
-	$filename = array();
-	$file2 = array();
-	$i = 0;
-		foreach ($_FILES["file"]["error"] as $key => $error) {
-			if ($error == UPLOAD_ERR_OK) {
-				$filename[$i][0] = $_FILES["file"]["tmp_name"][$key];
-				$filename[$i][1] = $_FILES["file"]["name"][$key];
-				$i++;
-			}
-		}
-	}
-
-	$body = "Имя: ".$name."\r\n";
-    $body .= "Возраст: ".$age."\r\n";
-    $body .= "Город проживания: ".$city."\r\n";
-    $body .= "Номер телефона: ".$tel."\r\n";
-    $body .= "Инстаграм: ".$insta."\r\n";
-    $body .= "Опыт в данной сфере: ".$exp;
-	send_mail($to, $body, $email, $filename);
-}
-
-
-
-
-// Вспомогательная функция для отправки почтового сообщения с вложением
-function send_mail($to, $body, $email, $filename)
-{
-	$subject = 'Заявка с сайта PremiumEscort';
-	$boundary = "--".md5(uniqid(time())); // генерируем разделитель
-	$headers = "From: ".$email."\r\n";	 
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .="Content-Type: multipart/mixed; boundary=\"".$boundary."\"\r\n";
-	$multipart = "--".$boundary."\r\n";
-	$multipart .= "Content-type: text/plain; charset=\"utf-8\"\r\n";
-	$multipart .= "Content-Transfer-Encoding: quoted-printable\r\n\r\n";
-
-	$body = $body."\r\n\r\n";
+if (isset ($_POST['name'])) {
+  $to = "matvienkoigor956@gmail.com";
+  $from = "support@tpverstak.ru";
+  $subject = "Заполнена контактная форма на сайте ".$_SERVER['HTTP_REFERER'];
+  
+  $message = "\nИмя пользователя: ".$_POST['name'];
+  $message .= "\nВозраст: ".$_POST['age'];
+  $message .= "\nТелефон: ".$_POST['tel'];
+  $message .= "\nГород: ".$_POST['city'];
+  $message .= "\nИнстаграм: ".$_POST['insta'];
+  $message .= "\nОпыт: ".$_POST['exp'];
+  // $message .= "\n\nАдрес сайта: ".$_SERVER['HTTP_REFERER'];
  
-	$multipart .= $body;
-	foreach ($filename as $key => $value) {
-		$fp = fopen($value[0], "r"); 
-		$content = fread($fp, filesize($value[0]));
-		fclose($fp);
-		$file .= "--".$boundary."\r\n";
-		$file .= "Content-Type: application/octet-stream\r\n";
-		$file .= "Content-Transfer-Encoding: base64\r\n";
-		$file .= "Content-Disposition: attachment; filename=\"".$value[1]."\"\r\n\r\n";
-		$file .= chunk_split(base64_encode($content))."\r\n";
-	}
-	$multipart .= $file."--".$boundary."--\r\n";
-	mail($to, $subject, $multipart, $headers);
+  
+  $boundary = md5(date('r', time()));
+  $filesize = '';
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "From: " . $from . "\r\n";
+  $headers .= "Reply-To: " . $from . "\r\n";
+  $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
+
+
+
+
+  $message="
+Content-Type: multipart/mixed; boundary=\"$boundary\"
+ 
+--$boundary
+Content-Type: text/plain; charset=\"utf-8\"
+Content-Transfer-Encoding: 7bit
+ 
+$message";
+
+
+
+
+
+     if(is_uploaded_file($_FILES['fileFF']['tmp_name'])) {
+         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['fileFF']['tmp_name'])));
+         $filename = $_FILES['fileFF']['name'];
+         $filetype = $_FILES['fileFF']['type'];
+         $filesize = $_FILES['fileFF']['size'];
+         $message.="
+ 
+--$boundary
+Content-Type: \"$filetype\"; name=\"$filename\"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=\"$filename\"
+ 
+$attachment";
+     }
+
+
+// for($i=0;$i<2;$i++){ 
+//           $file_name = $_FILES['fileFF']['name'];
+//           $filesize = $_FILES['fileFF']['size'];           
+//           $filetype = $_FILES['fileFF']['type'];
+//           $message .= "--$boundary\n"; 
+          
+//           $message .= "Content-Type: \"$filetype\"; name=\"$file_name\"
+//           Content-Disposition: attachment; filename=\"$file_name\";
+//           Content-Transfer-Encoding: base64"; 
+//  } 
+
+
+
+
+
+
+//    $message.="
+// --$boundary--";
+ 
+  if ($filesize < 10000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
+    mail($to, $subject, $message, $headers);
+    echo $_POST['name'].', Ваше сообщение отправлено, спасибо!';
+  } else {
+    echo 'Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.';
+  }
 }
 ?>
